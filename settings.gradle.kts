c7d1b92fc60ff0dbc2f700531eebf2dc53eea014
Štěpán Nezbeda <kyngs@users.noreply.github.com>
2026-08-18 12:39:25 +0200

    Remove bungee support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
 rootProject.name = "libby"
 include(":libby-paper")
 include(":libby-core")
-include(":libby-bungee")
 include(":libby-velocity")
 project(":libby-paper").projectDir = file("paper")
 project(":libby-core").projectDir = file("core")
-project(":libby-bungee").projectDir = file("bungee")
 project(":libby-velocity").projectDir = file("velocity")
